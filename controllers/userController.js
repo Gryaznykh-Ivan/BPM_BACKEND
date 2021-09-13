65fc1482039232a97c57e02e728ee8358c753c72
gryaznykh.ivan <gryaznykh.ivan@gmail.com>
2021-09-13 10:00:19 +0300

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,52 +1,106 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const validator = require('validator');
-const jwt = require('jsonwebtoken')
-const { user, bit } = require('../models')
+const jwt = require('jsonwebtoken');
+const { v4: uuid } = require('uuid');
+const { User, Refresh_token } = require('../models');
 
-const generateJwt = (id, email, role) => {
+const generateJwt = (id, email) => {
     return jwt.sign(
-        {id, email, role},
-        process.env.SECRET_KEY,
-        {expiresIn: '15m'}
+        { id, email },
+        process.env.SECRET,
+        { expiresIn: '15m' }
     )
 }
 
 const register = async ctx => {
     const { email, password } = ctx.request.body;
 
-    if (!validator.isEmail(email))
-    {
-        ctx.status = 400;
-        ctx.body = { success: false }
-        return
+    if (!validator.isEmail(email)) {
+        return ctx.throw(400, "Email указан неверно")
     }
 
-    const User = await user.findOne({ where: { name: email } });
-    if (User) {
-        ctx.status = 400;
-        ctx.body = { success: false }
-        return
+    if (!validator.isStrongPassword(password)) {
+        return ctx.throw(400, "Пароль должен содержать не менее 1 символa, не менее 1 цыфры, не менее 1 буквы в верхнем регистре")
+    }
+
+    const user = await User.findOne({ where: { name: email } });
+    if (user) {
+        return ctx.throw(400, "Пользователь с таким email уже существует")
     }
 
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
     try {
-        await user.create({name: email, password: hash });
+        const user = await User.create({ name: email, password: hash });
 
-        ctx.body = { success: true }
-    } catch(err) {
+        const refresh = uuid();
+        await Refresh_token.create({ token: refresh, user_id: user.user_id });
+
+        ctx.body = {
+            success: true,
+            token: generateJwt(user.user_id, user.name),
+            refresh_token: refresh
+        }
+    } catch (err) {
         console.log(err);
-        ctx.throw(400, 'Account was not created');
+        ctx.throw(400, 'Ошибка создания аккаунта');
     };
 }
 
 const login = async ctx => {
+    const { email, password } = ctx.request.body;
 
+    const user = await User.findOne({ where: { name: email } });
+    if (!user) {
+        return ctx.throw(400, "Пользователя с таким email не существует")
+    }
+
+    const comparePassword = bcrypt.compareSync(password, user.password)
+    if (!comparePassword) {
+        return ctx.throw(400, "Пароль введен неверно")
+    }
+
+    try {
+        const refresh = uuid();
+        await Refresh_token.create({ token: refresh, user_id: user.user_id });
+
+        ctx.body = {
+            success: true,
+            token: generateJwt(user.user_id, user.name),
+            refresh_token: refresh
+        }
+    } catch (err) {
+        console.log(err);
+        ctx.throw(400, 'Ошибка авторизации');
+    };
 }
 
 const refresh = async ctx => {
+    const { refresh_token } = ctx.request.body;
 
+    const refresh = await Refresh_token.findOne({
+        where: { token: refresh_token },
+        include: { model: User, as: "User" }
+    });
+
+    if (!refresh || !refresh.User) {
+        ctx.throw(400, 'Refresh_token указан неверно');
+    }
+
+    try {
+        const newRefresh = uuid();
+        await refresh.update({ date: Date.now(), token: newRefresh });
+
+        ctx.body = {
+            success: true,
+            token: generateJwt(refresh.User.user_id, refresh.User.name),
+            refresh_token: newRefresh
+        }
+    } catch (err) {
+        console.log(err);
+        ctx.throw(400, 'Ошибка создания новой пары токенов');
+    };
 }
 
 module.exports = {
--- models/index.js
+++ models/index.js
@@ -1,50 +1,50 @@
 const sequelize = require('../db')
 const DataTypes = require("sequelize").DataTypes;
-const bit = require("./bit")(sequelize, DataTypes);
-const bitmaker = require("./bitmaker")(sequelize, DataTypes);
-const bits_history = require("./bits_history")(sequelize, DataTypes);
-const box = require("./box")(sequelize, DataTypes);
-const faq = require("./faq")(sequelize, DataTypes);
-const license = require("./license")(sequelize, DataTypes);
-const license_bit = require("./license_bit")(sequelize, DataTypes);
-const license_type = require("./license_type")(sequelize, DataTypes);
-const refresh_token = require("./refresh_token")(sequelize, DataTypes);
-const replenishment_history = require("./replenishment_history")(sequelize, DataTypes);
-const user = require("./user")(sequelize, DataTypes);
+const Bit = require("./bit")(sequelize, DataTypes);
+const Bitmaker = require("./Bitmaker")(sequelize, DataTypes);
+const Bits_history = require("./Bits_history")(sequelize, DataTypes);
+const Box = require("./Box")(sequelize, DataTypes);
+const Faq = require("./Faq")(sequelize, DataTypes);
+const License = require("./License")(sequelize, DataTypes);
+const License_bit = require("./License_bit")(sequelize, DataTypes);
+const License_type = require("./License_type")(sequelize, DataTypes);
+const Refresh_token = require("./Refresh_token")(sequelize, DataTypes);
+const Replenishment_history = require("./Replenishment_history")(sequelize, DataTypes);
+const User = require("./User")(sequelize, DataTypes);
 
-bit.belongsToMany(license, { as: 'license_id_licenses', through: license_bit, foreignKey: "bit_id", otherKey: "license_id" });
-license.belongsToMany(bit, { as: 'bit_id_bits', through: license_bit, foreignKey: "license_id", otherKey: "bit_id" });
-bits_history.belongsTo(bit, { as: "bit", foreignKey: "bit_id" });
-bit.hasMany(bits_history, { as: "bits_histories", foreignKey: "bit_id" });
-license_bit.belongsTo(bit, { as: "bit", foreignKey: "bit_id" });
-bit.hasMany(license_bit, { as: "license_bits", foreignKey: "bit_id" });
-bit.belongsTo(bitmaker, { as: "author_bitmaker", foreignKey: "author" });
-bitmaker.hasMany(bit, { as: "bits", foreignKey: "author" });
-license.belongsTo(box, { as: "case", foreignKey: "case_id" });
-box.hasMany(license, { as: "licenses", foreignKey: "case_id" });
-license_bit.belongsTo(license, { as: "license", foreignKey: "license_id" });
-license.hasMany(license_bit, { as: "license_bits", foreignKey: "license_id" });
-bits_history.belongsTo(license_type, { as: "license_type_license_type", foreignKey: "license_type" });
-license_type.hasMany(bits_history, { as: "bits_histories", foreignKey: "license_type" });
-license.belongsTo(license_type, { as: "type_license_type", foreignKey: "type" });
-license_type.hasMany(license, { as: "licenses", foreignKey: "type" });
-bits_history.belongsTo(user, { as: "user", foreignKey: "user_id" });
-user.hasMany(bits_history, { as: "bits_histories", foreignKey: "user_id" });
-refresh_token.belongsTo(user, { as: "user", foreignKey: "user_id" });
-user.hasMany(refresh_token, { as: "refresh_tokens", foreignKey: "user_id" });
-replenishment_history.belongsTo(user, { as: "user", foreignKey: "user_id" });
-user.hasMany(replenishment_history, { as: "replenishment_histories", foreignKey: "user_id" });
+Bit.belongsToMany(License, { as: 'License_id_Licenses', through: License_bit, foreignKey: "bit_id", otherKey: "License_id" });
+License.belongsToMany(Bit, { as: 'bit_id_bits', through: License_bit, foreignKey: "License_id", otherKey: "bit_id" });
+Bits_history.belongsTo(Bit, { as: "bit", foreignKey: "bit_id" });
+Bit.hasMany(Bits_history, { as: "bits_histories", foreignKey: "bit_id" });
+License_bit.belongsTo(Bit, { as: "bit", foreignKey: "bit_id" });
+Bit.hasMany(License_bit, { as: "License_bits", foreignKey: "bit_id" });
+Bit.belongsTo(Bitmaker, { as: "author_Bitmaker", foreignKey: "author" });
+Bitmaker.hasMany(Bit, { as: "bits", foreignKey: "author" });
+License.belongsTo(Box, { as: "case", foreignKey: "case_id" });
+Box.hasMany(License, { as: "Licenses", foreignKey: "case_id" });
+License_bit.belongsTo(License, { as: "License", foreignKey: "License_id" });
+License.hasMany(License_bit, { as: "License_bits", foreignKey: "License_id" });
+Bits_history.belongsTo(License_type, { as: "License_type_License_type", foreignKey: "License_type" });
+License_type.hasMany(Bits_history, { as: "bits_histories", foreignKey: "License_type" });
+License.belongsTo(License_type, { as: "type_License_type", foreignKey: "type" });
+License_type.hasMany(License, { as: "Licenses", foreignKey: "type" });
+Bits_history.belongsTo(User, { as: "User", foreignKey: "User_id" });
+User.hasMany(Bits_history, { as: "bits_histories", foreignKey: "User_id" });
+Refresh_token.belongsTo(User, { as: "User", foreignKey: "User_id" });
+User.hasMany(Refresh_token, { as: "Refresh_tokens", foreignKey: "User_id" });
+Replenishment_history.belongsTo(User, { as: "User", foreignKey: "User_id" });
+User.hasMany(Replenishment_history, { as: "replenishment_histories", foreignKey: "User_id" });
 
 module.exports = {
-  bit,
-  bitmaker,
-  bits_history,
-  box,
-  faq,
-  license,
-  license_bit,
-  license_type,
-  refresh_token,
-  replenishment_history,
-  user,
+  Bit,
+  Bitmaker,
+  Bits_history,
+  Box,
+  Faq,
+  License,
+  License_bit,
+  License_type,
+  Refresh_token,
+  Replenishment_history,
+  User,
 };
