839aeafd25229acf6935c8dc2ae4aea5fbac01a1
gryaznykh.ivan <gryaznykh.ivan@gmail.com>
2021-09-14 20:33:34 +0300

    privateRouter

--- routes/privateApiRouter.js
+++ routes/privateApiRouter.js
@@ -1,10 +1,14 @@
 const Router = require('koa-router');
+const userController = require('../controllers/userController');
 
 const router = new Router().prefix('/api');
 
-router.post('/getLessons', async ctx => {
-    
-});
+// userController
+router.get('/user/profile', userController.profile);
+router.get('/user/replenishment_history', userController.replenishment_history);
+router.get('/user/bits_history', userController.bits_history);
+router.put('/user/changeName', userController.changeName);
+router.put('/user/changePhoto', userController.changePhoto);
 
 
 
